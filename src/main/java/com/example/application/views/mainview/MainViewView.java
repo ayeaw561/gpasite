ad0a088fb6d25c932cf382847dbe205f658a59fe
start.vaadin.com <no-reply@vaadin.com>
2021-12-04 18:28:52 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/mainview/MainViewView.java
@@ -0,0 +1,33 @@
+package com.example.application.views.mainview;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("MainView")
+@Route(value = "mainview", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class MainViewView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public MainViewView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
